e5382f0fc28d4b014ca56efb9638d5ef04b991e0
kanishka9r <kanishkarajput4918@gmail.com>
2026-07-04 20:31:46 +0530

    scedhuler added

--- main.py
+++ main.py
@@ -1,7 +1,20 @@
 from fastapi import FastAPI
 from recommender import get_ml_recommendations
+from apscheduler.schedulers.background import BackgroundScheduler
+from train_model import train_model
+import os
+
 
 app = FastAPI()
+
+@app.on_event("startup")
+def start_scheduler():
+    should_run = os.getenv("ENABLE_AUTO_TRAINING", "false")
+    if should_run.lower() == "true":
+        scheduler = BackgroundScheduler()
+        # Schedule the math to run every single night at 2:00 AM!
+        scheduler.add_job(train_model, 'cron', hour=2, minute=0)
+        scheduler.start()
 
 @app.get("/")
 def home():
